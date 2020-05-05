769b3a41ff730daf3942d9ef82728dc2a7799db3
Maja Francetic <francetic.maja@gmail.com>
2020-05-05 11:08:24 +0200

    Create a counterTextView, reset button and increment button and add their functionalities

--- app/src/main/java/com/example/peoplecounterwearapp/MainActivity.java
+++ app/src/main/java/com/example/peoplecounterwearapp/MainActivity.java
@@ -2,18 +2,43 @@ package com.example.peoplecounterwearapp;
 
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends WearableActivity {
 
-    private TextView mTextView;
+    private TextView counterTextView;
+    private Button resetButton;
+    private Button incrementButton;
+    private int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextView = (TextView) findViewById(R.id.text);
+        counterTextView = findViewById(R.id.counter_text_view);
+        resetButton = findViewById(R.id.reset_button);
+        incrementButton = findViewById(R.id.increment_button);
+
+        counterTextView.setText("0");
+
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter = 0;
+                counterTextView.setText(String.valueOf(counter));
+            }
+        });
+
+        incrementButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter++;
+                counterTextView.setText(String.valueOf(counter));
+            }
+        });
 
         // Enables Always-on
         setAmbientEnabled();
